5c51b35d903dbc783175e7edbd577475c2af355e
anvii2702 <anveshab02@gmail.com>
2025-03-25 17:14:07 +0530

    update

--- src/Components/About/About1.jsx
+++ src/Components/About/About1.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import About from '../images/About.png'
+import { Link } from 'react-router-dom'
+
 
 const About1 = () => {
   return (
     <div className='about1'>
        <img className='AboutImage' src={About}/>
+       <div className='aboutText'>
+        <div className='upper'>About Us</div>
+        <span><Link to="/home" className='textlink'>HOME</Link></span>  &gt;
+          <span><Link to="/about" className='textlink'>  ABOUT US</Link></span>
+       </div>
     </div>
   )
 }
--- src/Components/Contact/Contact1.jsx
+++ src/Components/Contact/Contact1.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import About from '../images/About.png'
 import '../csss/ContactCSS/Contact1.css'
+import { Link } from 'react-router-dom'
 
 
 const Contact1 = () => {
   return (
     <div className='contactImage'>
         <img className='contactImage' src={About} />
+        <div className='aboutText'>
+        <div className='upper'>Contact Us</div>
+        <span><Link to="/home" className='textlink'>HOME</Link></span>  &gt;
+          <span><Link to="/contact" className='textlink'>  CONTACT US</Link></span>
+       </div>
     </div>
   )
 }
